92f399e25afc4c1d39003374ec44a0b8f0e9f2ad
Bri Masse <BriMasse090303@ATT.net>
2019-01-13 13:46:40 -0500

    Initial camera widget

---
+++ Dashboard/Dashboard/Camera.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Dashboard
+{
+    /// <summary>
+    /// Interaction logic for Camera.xaml
+    /// </summary>
+    public partial class Camera : UserControl
+    {
+        public Camera()
+        {
+            InitializeComponent();
+            wbSample.Navigate("http://www.wpf-tutorial.com");
+        }
+    }
+}
