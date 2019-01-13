39dbc01d31bb43461be203a8b9245de7c09b80a0
PatFagan <patrickfagan1586@gmail.com>
2019-01-13 14:18:19 -0500

    Added "Current Meter" widget.

---
+++ Dashboard/Dashboard/CurrentMeter.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dashboard
+{
+    /// <summary>
+    /// Interaction logic for CurrentMeter.xaml
+    /// </summary>
+    public partial class CurrentMeter : UserControl
+    {
+        public CurrentMeter()
+        {
+            InitializeComponent();
+            //Motor0.Value = 0;
+            //Motor1.Value = 0;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
