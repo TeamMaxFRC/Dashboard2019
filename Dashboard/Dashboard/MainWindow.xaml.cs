75d9c0530f15566112eecbcc0fc2224c872ec164
PatFagan <patrickfagan1586@gmail.com>
2019-01-22 19:16:28 -0500

    Adjusted CurrentMeter motor value names to match robot code

--- Dashboard/Dashboard/MainWindow.xaml.cs
+++ Dashboard/Dashboard/MainWindow.xaml.cs
@@ -48,14 +48,29 @@ namespace Dashboard
                     if (ReceivedMessage == null) continue;
 
                     // Show any received motor values.
-                    if (ReceivedMessage.Address.Equals("/Robot/Motors/Left/Value"))
+                    if (ReceivedMessage.Address.Equals("/Robot/Motors/LeftMaster/Value"))
                     {
-                        Application.Current.Dispatcher.InvokeAsync(new Action(() => CurrentWidget.SetLeftMotorValue((double)ReceivedMessage.Arguments[0])));
+                        Application.Current.Dispatcher.InvokeAsync(new Action(() => CurrentWidget.SetLeftMasterMotorValue((double)ReceivedMessage.Arguments[0])));
                     }
-
-                    if (ReceivedMessage.Address.Equals("/Robot/Motors/Right/Value"))
+                    if (ReceivedMessage.Address.Equals("/Robot/Motors/LeftSlavePrimary/Value"))
                     {
-                        Application.Current.Dispatcher.InvokeAsync(new Action(() => CurrentWidget.SetRightMotorValue((double)ReceivedMessage.Arguments[0])));
+                        Application.Current.Dispatcher.InvokeAsync(new Action(() => CurrentWidget.SetLeftSlavePrimaryMotorValue((double)ReceivedMessage.Arguments[0])));
+                    }
+                    if (ReceivedMessage.Address.Equals("/Robot/Motors/LeftSlaveSecondary/Value"))
+                    {
+                        Application.Current.Dispatcher.InvokeAsync(new Action(() => CurrentWidget.SetLeftSlaveSecondaryMotorValue((double)ReceivedMessage.Arguments[0])));
+                    }
+                    if (ReceivedMessage.Address.Equals("/Robot/Motors/RightMaster/Value"))
+                    {
+                        Application.Current.Dispatcher.InvokeAsync(new Action(() => CurrentWidget.SetRightMasterMotorValue((double)ReceivedMessage.Arguments[0])));
+                    }
+                    if (ReceivedMessage.Address.Equals("/Robot/Motors/RightSlavePrimary/Value"))
+                    {
+                        Application.Current.Dispatcher.InvokeAsync(new Action(() => CurrentWidget.SetRightSlavePrimaryMotorValue((double)ReceivedMessage.Arguments[0])));
+                    }
+                    if (ReceivedMessage.Address.Equals("/Robot/Motors/RightSlaveSecondary/Value"))
+                    {
+                        Application.Current.Dispatcher.InvokeAsync(new Action(() => CurrentWidget.SetRightSlaveSecondaryMotorValue((double)ReceivedMessage.Arguments[0])));
                     }
                     //if (RecievedMessaage.Address.Equals("Error"))
                     //Application.ErrorReporter.Dispatcher.InvokeAsync(new Action(() => ErrorReporter.SendError((double)Arguments[0])));
