0aa6b145a4f5dea2c59827d0839073b0cac356cf
Ivy <icbrowne04@gmail.com>
2019-02-02 14:24:51 -0500

    Changing console. Was same thing twice

--- Dashboard/Dashboard/Console.xaml.cs
+++ Dashboard/Dashboard/Console.xaml.cs
@@ -15,22 +15,13 @@ namespace Dashboard
             InitializeComponent();
         }
 
-        //Will connect robot to console widget.
-      
-            public void SetError1(String Message, bool Add)
-            if (Add)
-           {
-               ConsoleBox.Text += Message + "\n";
-         
-           }
-        {
- 
-
         // Prints a line to the console.
         public void PrintLine(String Line)
         {
             ConsoleBox.Text += "\n" + Line;
             ConsoleBox.ScrollToEnd();
         }
+        
     }
 }
+
