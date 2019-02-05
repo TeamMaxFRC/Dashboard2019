16edc756f389c4e55a98c49661509611bfc23bb4
Matthew Iasevoli <miasevoli.frc@gmail.com>
2019-02-05 18:19:26 -0500

    New and improved Limelight code

--- Dashboard/Dashboard/LimeLight.xaml.cs
+++ Dashboard/Dashboard/LimeLight.xaml.cs
@@ -1,28 +1,28 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Dashboard
 {
     /// <summary>
-    /// Interaction logic for CurrentMeter.xaml
+    /// Interaction logic for Limelight.xaml
     /// </summary>
-    public partial class LimeLight : UserControl
+    public partial class Limelight : Page
     {
-        public LimeLight()
+        public Limelight()
         {
             InitializeComponent();
-        }
-
-        public void RefreshPage()
-        {
-            bool test = LimeLightWebView.IsLoaded;
-            if (test)
-            {
-                Debug.Content = "Loaded!";
-            }
-            else
-            {
-                Debug.Content = "Nope!";
-            }
         }
     }
 }
