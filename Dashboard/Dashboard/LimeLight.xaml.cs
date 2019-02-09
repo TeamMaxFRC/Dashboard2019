4b1ed2e6187a5b3e1693b61d866e9f556caf6c52
Matthew Iasevoli <miasevoli.frc@gmail.com>
2019-02-09 14:10:45 -0500

    Finally got a working dashboard camera.  Still needs some tweaks, but it works.

--- Dashboard/Dashboard/LimeLight.xaml.cs
+++ Dashboard/Dashboard/LimeLight.xaml.cs
@@ -1,28 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MjpegProcessor;
 
 namespace Dashboard
 {
     /// <summary>
     /// Interaction logic for Limelight.xaml
     /// </summary>
+    /// 
+
     public partial class Limelight : UserControl
     {
+        readonly MjpegDecoder _mjpeg;
+
         public Limelight()
         {
             InitializeComponent();
+            _mjpeg = new MjpegDecoder();
+            _mjpeg.FrameReady += mjpeg_FrameReady;
+            _mjpeg.Error += _mjpeg_Error;
+            _mjpeg.ParseStream(new Uri("http://limelight.local:5800"));
+        }
+
+        private void mjpeg_FrameReady(object sender, FrameReadyEventArgs e)
+        {
+            MJpegView.Source = e.BitmapImage;
+        }
+
+        void _mjpeg_Error(object sender, ErrorEventArgs e)
+        {
+            MessageBox.Show(e.Message);
         }
 
         public void UpdateX(double X)
